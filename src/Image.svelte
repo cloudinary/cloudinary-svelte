bf64ddf2ca2150c933c353f628d3c609b3591e3c
Nir Maoz <47939921+maoznir@users.noreply.github.com>
2020-06-28 12:48:04 +0300

    Add support for responsive image (#3)

--- src/Image.svelte
+++ src/Image.svelte
@@ -1,10 +1,26 @@
 <script>
-  /**
-   * Cloudinary Image Component
-   */
+    /**
+     * Cloudinary Image Component
+     * This element binds the underlying <img> element to it's imgElement variable.
+     * imgElement is passed to makeElementResponsive() so it can update the src attribute when needed
+     */
+    import {afterUpdate} from 'svelte';
+    import {getImageTag, makeElementResponsive} from "./utils";
 
-  import { getImageTag } from "./utils";
-  $: imageTag = getImageTag($$props);
+    /**
+     * Bind imgElement to the underlying <img> element.
+     * @param node - the underlying <img> element's node.
+     */
+    const bindImage = (node) => (imgElement = node);
+
+    let imgElement; // Reference to underlying <img> element
+    $: attributes = getImageTag($$props).attributes(); // The <img> attributes, computed on update.
+
+    afterUpdate(() => {
+        if ($$props.responsive && imgElement) {
+            makeElementResponsive(imgElement, $$props);
+        }
+    });
 </script>
 
-<img alt="" {...imageTag.attributes()} />
+<img use:bindImage alt="" {...attributes}/>
